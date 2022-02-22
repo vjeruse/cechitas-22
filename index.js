46a33f289181750d9a8ea5ddddefbd86fa7f5efb
vjeruse <v.nguyenova@gmail.com>
2022-02-22 17:48:21 +0100

    1 file modified

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 //import './style.css'; jednoradkovy komentar
 /* vice radkovy komentar
-*/
+
+text = string
+
+
 console.log("Ahoj svete")
 import'./ukol1'
 prompt("Kolik je ti let?")
 alert('neser')
+*/
+let jmeno;
+let vek = 25;
+let svobody = false;
+
+jmeno = "Vera";
+console.log(jmeno);
