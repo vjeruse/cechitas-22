68e353941ae0b2736eb9258925a8f72a7aa452cb
vjeruse <v.nguyenova@gmail.com>
2022-02-22 17:51:45 +0100

    1 file modified

--- index.js
+++ index.js
@@ -2,16 +2,22 @@
 /* vice radkovy komentar
 
 text = string
-
+vice hodnot - array
+object
 
 console.log("Ahoj svete")
 import'./ukol1'
 prompt("Kolik je ti let?")
 alert('neser')
 */
-let jmeno;
+/*let jmeno;
 let vek = 25;
 let svobody = false;
 
 jmeno = "Vera";
 console.log(jmeno);
+*/
+let a = 5;
+let b = 100;
+let soucet = a + b + 100;
+console.log(soucet);
