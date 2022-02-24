1d5b588170c221f50a89719581c5dc4ad87f5eb6
vjeruse <v.nguyenova@gmail.com>
2022-02-24 17:17:50 +0100

    1 file modified

--- ukol5.js
+++ ukol5.js
@@ -3,8 +3,8 @@ let soucet = 0
 let cislo = Number (prompt("Zadej cislo:"))
 
 while (cislo !==0){
-  cislo = Number (prompt("Zadej cislo:"))
   soucet += cislo //soucet + cislo
   console.log("zadane cislo:" + cislo)
-
-console.log("Naakumulovany soucet:"+ soucet)}
+  console.log("Naakumulovany soucet:"+ soucet)
+  cislo = Number (prompt("Zadej cislo:"))
+}
